fe4faa5d4e00d36a48244fb2618e44c291038d35
Sijie Shen <shensijie.ssj@alibaba-inc.com>
2023-11-16 14:26:19 +0800

    Add check_process.sh script and update bulk load data logging
    Signed-off-by: Sijie Shen <shensijie.ssj@alibaba-inc.com>

---
+++ scripts/check_process.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# Define the list of pids with process names
+
+# $1 format: declare -A pids=([key2]="value2" [key1]="value1" )
+
+eval "$1"
+
+for process_name in ${!pids[@]}; do
+    process_pid=${pids[$process_name]}
+    if [[ -z $process_pid || ! $process_pid =~ ^[0-9]+$ ]]; then
+        echo "Invalid pid $process_pid for process $process_name"
+        unset pids[$process_name]
+        continue
+    fi
+done
+
+if [[ ${#pids[@]} -eq 0 ]]; then
+    echo "No valid pids found"
+    exit -1
+fi
+
+while true; do
+    # Loop through the list of pids and process names
+    for process_name in ${!pids[@]}; do
+        process_pid=${pids[$process_name]}
+
+        # echo "Checking $process_name with pid $process_pid"
+
+        # Check if the process is running
+        if ! ps -p "$process_pid" > /dev/null; then
+            echo "$process_name is not running"
+            ./stop-gart
+        fi
+    done
+
+    # Wait for 10 seconds and repeat the process
+    sleep 10
+done
