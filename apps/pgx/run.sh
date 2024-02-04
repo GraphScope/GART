d4ff546b709600234adb884d98bd2f2065fc487b
Sijie Shen <shensijie.ssj@alibaba-inc.com>
2024-02-04 17:53:30 +0800

    Unified GART parameters in PGX
    Signed-off-by: Sijie Shen <shensijie.ssj@alibaba-inc.com>

--- apps/pgx/run.sh
+++ apps/pgx/run.sh
@@ -17,14 +17,30 @@ parse_ini() {
 
 KAFKA_HOME=$(parse_ini "path" "KAFKA_HOME" $config_file)
 GART_HOME=$(parse_ini "path" "GART_HOME" $config_file)
+real_time_log_path=$(parse_ini "log" "real_time_log_path" $config_file)
+
+echo "GART_HOME: $GART_HOME"
+echo "KAFKA_HOME: $KAFKA_HOME"
+echo "real_time_log_path: $real_time_log_path"
+
+db_type=$(parse_ini "gart" "db-type" $config_file)
+rgmapping=$(parse_ini "gart" "rgmapping-file" $config_file)
+v6d_sock=$(parse_ini "gart" "v6d-sock" $config_file)
+etcd_endpoints=$(parse_ini "gart" "etcd-endpoints" $config_file)
+subgraph_num=$(parse_ini "gart" "subgraph-num" $config_file)
+enable_bulkload=$(parse_ini "gart" "enable-bulkload" $config_file)
 
 echo "username: $username"
 echo "password: $password"
-echo "GART_HOME: $GART_HOME"
-echo "KAFKA_HOME: $KAFKA_HOME"
+echo "db_type: $db_type"
+echo "rgmapping: $rgmapping"
+echo "v6d_sock: $v6d_sock"
+echo "etcd_endpoints: $etcd_endpoints"
+echo "subgraph_num: $subgraph_num"
+echo "enable_bulkload: $enable_bulkload"
 
 (export KAFKA_HOME=$KAFKA_HOME; cd $GART_HOME/build; \
-./gart --db-type postgresql -u $username -p $password -r schema/rgmapping-ldbc.yaml --v6d-sock ldbc.sock -e 127.0.0.1:23760 --subgraph-num 4 --enable-bulkload 1 2>&1 \
- | tee /opt/postgresql/gart.log &)
+./gart --db-type $db_type -u $username -p $password -r $rgmapping --v6d-sock $v6d_sock -e $etcd_endpoint --subgraph-num $subgraph_num --enable-bulkload $enable_bulkload 2>&1 \
+ | tee $real_time_log_path &)
 
 # (export KAFKA_HOME=$KAFKA_HOME; cd $GART_HOME/build;./stop-gart --kill-v6d-sock ldbc.sock)
