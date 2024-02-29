d2f221bd7ab4d61c01c0a5720d9953171478568c
Sijie Shen <shensijie.ssj@alibaba-inc.com>
2024-02-29 17:55:48 +0800

    Add algorithm router with parser
    Signed-off-by: Sijie Shen <shensijie.ssj@alibaba-inc.com>

--- apps/networkx/client/test/algorithm.py
+++ apps/networkx/client/test/algorithm.py
@@ -15,13 +15,26 @@ def get_parser():
         "--host", default="127.0.0.1", help="NetworkX server host address"
     )
     parser.add_argument("--port", default=50051, help="NetworkX server port")
-    parser.add_argument(
-        "--algorithm", default="sssp", help="NetworkX algorithm (sssp, ...)"
-    )
-    parser.add_argument(
-        "--args", default="(0, 1)", help="Args for the algorithm (e.g. (0, 1) for sssp)"
-    )
     parser.add_argument("--output", default="output", help="Output file name")
+
+    subparsers = parser.add_subparsers(dest="algorithm", help="graph algorithm to run")
+
+    parser_sssp = subparsers.add_parser("sssp", help="Single Source Shortest Path")
+    parser_sssp.add_argument(
+        "--source", default="(0, 0)", help="The source node id (default: %(default)s)"
+    )
+
+    parser_wsssp = subparsers.add_parser(
+        "wsssp", help="Weighted Single Source Shortest Path"
+    )
+    parser_wsssp.add_argument(
+        "--source", default="(0, 0)", help="The source node id (default: %(default)s)"
+    )
+    parser_wsssp.add_argument(
+        "--weight",
+        default="wa_work_from",
+        help="The weight property (default: %(default)s)",
+    )
 
     return parser
 
@@ -31,8 +44,20 @@ if __name__ == "__main__":
     g = DiGraph(f"{args.host}:{args.port}")
 
     if args.algorithm.lower() == "sssp":
-        source = ast.literal_eval(args.args)
+        source = ast.literal_eval(args.source)
+        print(f"Running Single Source Shortest Path algorithm on {source} ...")
         length = nx.single_source_shortest_path_length(g, source)
+
+        with open(args.output, "w", encoding="UTF-8") as f:
+            for node, dist in length.items():
+                f.write(f"{node}: {dist}\n")
+
+    if args.algorithm.lower() == "wsssp":
+        source = ast.literal_eval(args.source)
+        print(
+            f"Running Weighted Single Source Shortest Path algorithm on {source} by {args.weight} ..."
+        )
+        length = nx.single_source_dijkstra_path_length(g, source, weight=args.weight)
 
         with open(args.output, "w", encoding="UTF-8") as f:
             for node, dist in length.items():
