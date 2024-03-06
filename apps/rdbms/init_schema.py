046386aef1c760c0f496ba801fd5e87a738ec843
Sijie Shen <shensijie.ssj@alibaba-inc.com>
2024-03-06 16:31:05 +0800

    Refine init schema
    Signed-off-by: Sijie Shen <shensijie.ssj@alibaba-inc.com>

--- apps/rdbms/init_schema.py
+++ apps/rdbms/init_schema.py
@@ -26,422 +26,247 @@ def get_parser():
     return parser
 
 
-arg_parser = get_parser()
-args = arg_parser.parse_args()
+def get_args():
+    args = get_parser().parse_args()
 
-unset = False
+    unset = False
 
-if not isinstance(args.user, str) or len(args.user) == 0:
-    print("Please specify the database user with --user")
-    unset = True
+    if not isinstance(args.user, str) or len(args.user) == 0:
+        print("Please specify the database user with --user")
+        unset = True
 
-if not isinstance(args.password, str) or len(args.password) == 0:
-    print("Please specify the database password with --password")
-    unset = True
+    if not isinstance(args.password, str) or len(args.password) == 0:
+        print("Please specify the database password with --password")
+        unset = True
 
-if unset:
-    sys.exit(1)
+    if unset:
+        sys.exit(1)
 
-print("Args: ", args)
+    return args
 
-print("\n========================================\n")
 
-if args.db_type == "mysql":
-    connection_string = "mysql+pymysql://%s:%s@%s:%s/" % (
-        args.user,
-        args.password,
-        args.host,
-        args.port,
-    )
-    engine = create_engine(connection_string, echo=False)
-elif args.db_type == "postgresql":
-    connection_string = "postgresql://%s:%s@%s:%s/%s" % (
-        args.user,
-        args.password,
-        args.host,
-        args.port,
-        args.db,
-    )
-    engine = create_engine(connection_string, echo=False)
-else:
-    print("We now only support mysql and postgresql")
-    exit(1)
+vertexTableDDL = {
+    "organisation": """
+        CREATE TABLE organisation (
+            org_id BIGINT NOT NULL,
+            org_type VARCHAR(255),
+            org_name TEXT,
+            org_url TEXT,
+            PRIMARY KEY (org_id)
+        )
+    """,
+    "place": """
+        CREATE TABLE place (
+            pla_id BIGINT NOT NULL,
+            pla_name TEXT,
+            pla_url TEXT,
+            pla_type VARCHAR(255),
+            PRIMARY KEY (pla_id)
+        )
+    """,
+    "tag": """
+        CREATE TABLE tag (
+            tag_id BIGINT NOT NULL,
+            tag_name TEXT,
+            tag_url TEXT,
+            PRIMARY KEY (tag_id)
+        )
+    """,
+    "tagclass": """
+        CREATE TABLE tagclass (
+            tagc_id BIGINT NOT NULL,
+            tagc_name TEXT,
+            tagc_url TEXT,
+            PRIMARY KEY (tagc_id)
+        )
+    """,
+    "person": """
+        CREATE TABLE person (
+            p_id BIGINT NOT NULL,
+            p_first_name VARCHAR(255),
+            p_last_name VARCHAR(255),
+            p_gender VARCHAR(255),
+            p_birthday VARCHAR(255),
+            p_creation_date VARCHAR(255),
+            p_location_ip VARCHAR(255),
+            p_browser_used VARCHAR(255),
+            PRIMARY KEY (p_id)
+        )
+    """,
+    "comment": """
+        CREATE TABLE comment (
+            co_id BIGINT NOT NULL,
+            co_creation_date VARCHAR(255),
+            co_location_ip VARCHAR(255),
+            co_browser_used VARCHAR(255),
+            co_content TEXT,
+            co_length INT,
+            PRIMARY KEY (co_id)
+        )
+    """,
+    "post": """
+        CREATE TABLE post (
+            po_id BIGINT NOT NULL,
+            po_image_file VARCHAR(255),
+            po_creation_date VARCHAR(255),
+            po_location_ip VARCHAR(255),
+            po_browser_used VARCHAR(255),
+            po_language VARCHAR(255),
+            po_content TEXT,
+            po_length INT,
+            PRIMARY KEY (po_id)
+        )
+    """,
+    "forum": """
+        CREATE TABLE forum (
+            fo_id BIGINT NOT NULL,
+            fo_title TEXT,
+            fo_creation_date VARCHAR(255),
+            PRIMARY KEY (fo_id)
+        )
+    """,
+}
 
-conn = engine.raw_connection()
-cursor = conn.cursor()
 
-# create the database
-if args.drop:
-    print(f"Drop database {args.db}")
-    if args.db_type == "mysql":
-        cursor.execute(f"DROP DATABASE IF EXISTS {args.db}")
-    elif args.db_type == "postgresql":
-        print("Not supported drop database for postgresql.")
+# Function to generate edge table SQL based on a given table name
+def generate_edge_ddl(table_name, additional_columns=""):
+    return f"""
+        CREATE TABLE {table_name} (
+            src BIGINT,
+            dst BIGINT,
+            {additional_columns}
+            PRIMARY KEY (src, dst)
+        )
+    """
 
-if args.db_type == "mysql":
-    cursor.execute(f"CREATE DATABASE IF NOT EXISTS {args.db}")
-    cursor.execute(f"USE {args.db}")
-# Not supported create database for postgresql
+
+# Dictionary of edge tables and their generated SQL statements
+simepleEdgeTables = [
+    "org_islocationin",
+    "ispartof",
+    "issubclassof",
+    "hastype",
+    "comment_hascreator",
+    "comment_hastag",
+    "comment_islocationin",
+    "replyof_comment",
+    "replyof_post",
+    "post_hascreator",
+    "post_hastag",
+    "post_islocationin",
+    "forum_containerof",
+    "forum_hasmoderator",
+    "forum_hastag",
+    "person_hasinterest",
+    "person_islocationin",
+    "forum_hasmember",
+]
+
+simpleEdgeTableDDL = {
+    table_name: generate_edge_ddl(table_name) for table_name in simepleEdgeTables
+}
+
+propertyEdgeTables = {
+    "knows": "pk_creation_date VARCHAR(255),",
+    "likes_comment": "likes_co_creation_date VARCHAR(255),",
+    "likes_post": "likes_po_creation_date VARCHAR(255),",
+    "studyat": "sa_class_year INT,",
+    "workat": "wa_work_from INT,",
+}
+
+propertyEdgeTableDDL = {
+    table_name: generate_edge_ddl(table_name, additional_columns=additional_columns)
+    for table_name, additional_columns in propertyEdgeTables.items()
+}
+
 
 # create vertex tables
-cursor.execute("DROP TABLE IF EXISTS organisation")
-sql = """CREATE TABLE organisation (
-        org_id BIGINT NOT NULL,
-        org_type VARCHAR(255),
-        org_name TEXT,
-        org_url TEXT,
-        PRIMARY KEY (org_id))"""
-cursor.execute(sql)
-
-print("Created vertex table: organisation")
-
-cursor.execute("DROP TABLE IF EXISTS place")
-sql = """CREATE TABLE place (
-        pla_id BIGINT NOT NULL,
-        pla_name TEXT,
-        pla_url TEXT,
-        pla_type VARCHAR(255),
-        PRIMARY KEY (pla_id))"""
-cursor.execute(sql)
-
-print("Created vertex table: place")
-
-cursor.execute("DROP TABLE IF EXISTS tag")
-sql = """CREATE TABLE tag (
-        tag_id BIGINT NOT NULL,
-        tag_name TEXT,
-        tag_url TEXT,
-        PRIMARY KEY (tag_id))"""
-cursor.execute(sql)
-
-print("Created vertex table: tag")
-
-cursor.execute("DROP TABLE IF EXISTS tagclass")
-sql = """CREATE TABLE tagclass (
-        tagc_id BIGINT NOT NULL,
-        tagc_name TEXT,
-        tagc_url TEXT,
-        PRIMARY KEY (tagc_id))"""
-cursor.execute(sql)
-
-print("Created vertex table: tagclass")
-
-cursor.execute("DROP TABLE IF EXISTS person")
-sql = """CREATE TABLE person (
-        p_id BIGINT NOT NULL,
-        p_first_name VARCHAR(255),
-        p_last_name VARCHAR(255),
-        p_gender VARCHAR(255),
-        p_birthday VARCHAR(255),
-        p_creation_date VARCHAR(255),
-        p_location_ip VARCHAR(255),
-        p_browser_used VARCHAR(255),
-        PRIMARY KEY (p_id)
-        )"""
-cursor.execute(sql)
-
-print("Created vertex table: person")
-
-cursor.execute("DROP TABLE IF EXISTS comment")
-sql = """CREATE TABLE comment (
-        co_id BIGINT NOT NULL,
-        co_creation_date VARCHAR(255),
-        co_location_ip VARCHAR(255),
-        co_browser_used VARCHAR(255),
-        co_content TEXT,
-        co_length INT,
-        PRIMARY KEY (co_id)
-        )"""
-cursor.execute(sql)
-
-print("Created vertex table: comment")
-
-cursor.execute("DROP TABLE IF EXISTS post")
-sql = """CREATE TABLE post (
-        po_id BIGINT NOT NULL,
-        po_image_file VARCHAR(255),
-        po_creation_date VARCHAR(255),
-        po_location_ip VARCHAR(255),
-        po_browser_used VARCHAR(255),
-        po_language VARCHAR(255),
-        po_content TEXT,
-        po_length INT,
-        PRIMARY KEY (po_id)
-        )"""
-cursor.execute(sql)
-
-print("Created vertex table: post")
-
-cursor.execute("DROP TABLE IF EXISTS forum")
-sql = """CREATE TABLE forum (
-        fo_id BIGINT NOT NULL,
-        fo_title TEXT,
-        fo_creation_date VARCHAR(255),
-        PRIMARY KEY (fo_id)
-        )"""
-cursor.execute(sql)
-
-print("Created vertex table: forum")
-
-# insert edge tables
-
-cursor.execute("DROP TABLE IF EXISTS org_islocationin")
-sql = """CREATE TABLE org_islocationin (
-        src BIGINT,
-        dst BIGINT,
-        PRIMARY KEY (src, dst)
-        )"""
-cursor.execute(sql)
-
-print("Created edge table: org_islocationin")
-
-cursor.execute("DROP TABLE IF EXISTS ispartof")
-sql = """CREATE TABLE ispartof (
-        src BIGINT,
-        dst BIGINT,
-        PRIMARY KEY (src, dst)
-        )"""
-cursor.execute(sql)
-
-print("Created edge table: ispartof")
-
-cursor.execute("DROP TABLE IF EXISTS issubclassof")
-sql = """CREATE TABLE issubclassof (
-        src BIGINT,
-        dst BIGINT,
-        PRIMARY KEY (src, dst)
-        )"""
-cursor.execute(sql)
-
-print("Created edge table: issubclassof")
-
-cursor.execute("DROP TABLE IF EXISTS hastype")
-sql = """CREATE TABLE hastype (
-        src BIGINT,
-        dst BIGINT,
-        PRIMARY KEY (src, dst)
-        )"""
-cursor.execute(sql)
-
-print("Created edge table: hastype")
-
-cursor.execute("DROP TABLE IF EXISTS comment_hascreator")
-sql = """CREATE TABLE comment_hascreator (
-        src BIGINT,
-        dst BIGINT,
-        PRIMARY KEY (src, dst)
-        )"""
-cursor.execute(sql)
-
-print("Created edge table: comment_hascreator")
-
-cursor.execute("DROP TABLE IF EXISTS comment_hastag")
-sql = """CREATE TABLE comment_hastag (
-        src BIGINT,
-        dst BIGINT,
-        PRIMARY KEY (src, dst)
-        )"""
-cursor.execute(sql)
-
-print("Created edge table: comment_hastag")
-
-cursor.execute("DROP TABLE IF EXISTS comment_islocationin")
-sql = """CREATE TABLE comment_islocationin (
-        src BIGINT,
-        dst BIGINT,
-        PRIMARY KEY (src, dst)
-        )"""
-cursor.execute(sql)
-
-print("Created edge table: comment_islocationin")
-
-cursor.execute("DROP TABLE IF EXISTS replyof_comment")
-sql = """CREATE TABLE replyof_comment (
-        src BIGINT,
-        dst BIGINT,
-        PRIMARY KEY (src, dst)
-        )"""
-cursor.execute(sql)
-
-print("Created edge table: replyof_comment")
-
-cursor.execute("DROP TABLE IF EXISTS replyof_post")
-sql = """CREATE TABLE replyof_post (
-        src BIGINT,
-        dst BIGINT,
-        PRIMARY KEY (src, dst)
-        )"""
-cursor.execute(sql)
-
-print("Created edge table: replyof_post")
-
-cursor.execute("DROP TABLE IF EXISTS post_hascreator")
-sql = """CREATE TABLE post_hascreator (
-        src BIGINT,
-        dst BIGINT,
-        PRIMARY KEY (src, dst)
-        )"""
-cursor.execute(sql)
-
-print("Created edge table: post_hascreator")
-
-cursor.execute("DROP TABLE IF EXISTS post_hastag")
-sql = """CREATE TABLE post_hastag (
-        src BIGINT,
-        dst BIGINT,
-        PRIMARY KEY (src, dst)
-        )"""
-cursor.execute(sql)
-
-print("Created edge table: post_hastag")
-
-cursor.execute("DROP TABLE IF EXISTS post_islocationin")
-sql = """CREATE TABLE post_islocationin (
-        src BIGINT,
-        dst BIGINT,
-        PRIMARY KEY (src, dst)
-        )"""
-cursor.execute(sql)
-
-print("Created edge table: post_islocationin")
-
-cursor.execute("DROP TABLE IF EXISTS forum_containerof")
-sql = """CREATE TABLE forum_containerof (
-        src BIGINT,
-        dst BIGINT,
-        PRIMARY KEY (src, dst)
-        )"""
-cursor.execute(sql)
-
-print("Created edge table: forum_containerof")
-
-cursor.execute("DROP TABLE IF EXISTS forum_hasmoderator")
-sql = """CREATE TABLE forum_hasmoderator (
-        src BIGINT,
-        dst BIGINT,
-        PRIMARY KEY (src, dst)
-        )"""
-cursor.execute(sql)
-
-print("Created edge table: forum_hasmoderator")
-
-cursor.execute("DROP TABLE IF EXISTS forum_hastag")
-sql = """CREATE TABLE forum_hastag (
-        src BIGINT,
-        dst BIGINT,
-        PRIMARY KEY (src, dst)
-        )"""
-cursor.execute(sql)
-
-print("Created edge table: forum_hastag")
-
-cursor.execute("DROP TABLE IF EXISTS person_hasinterest")
-sql = """CREATE TABLE person_hasinterest (
-        src BIGINT,
-        dst BIGINT,
-        PRIMARY KEY (src, dst)
-        )"""
-cursor.execute(sql)
-
-print("Created edge table: person_hasinterest")
-
-cursor.execute("DROP TABLE IF EXISTS person_islocationin")
-sql = """CREATE TABLE person_islocationin (
-        src BIGINT,
-        dst BIGINT,
-        PRIMARY KEY (src, dst)
-        )"""
-cursor.execute(sql)
-
-print("Created edge table: person_islocationin")
-
-cursor.execute("DROP TABLE IF EXISTS forum_hasmember")
-sql = """CREATE TABLE forum_hasmember (
-        src BIGINT,
-        dst BIGINT,
-        fo_hm_join_date VARCHAR(255),
-        PRIMARY KEY (src, dst)
-        )"""
-cursor.execute(sql)
-
-print("Created edge table: forum_hasmember")
-
-cursor.execute("DROP TABLE IF EXISTS knows")
-sql = """CREATE TABLE knows (
-        src BIGINT,
-        dst BIGINT,
-        kn_creation_date VARCHAR(255),
-        PRIMARY KEY (src, dst)
-        )"""
-cursor.execute(sql)
-
-print("Created edge table: knows")
-
-cursor.execute("DROP TABLE IF EXISTS likes_comment")
-sql = """CREATE TABLE likes_comment (
-        src BIGINT,
-        dst BIGINT,
-        likes_co_creation_date VARCHAR(255),
-        PRIMARY KEY (src, dst)
-        )"""
-cursor.execute(sql)
-
-print("Created edge table: likes_comment")
-
-cursor.execute("DROP TABLE IF EXISTS likes_post")
-sql = """CREATE TABLE likes_post (
-        src BIGINT,
-        dst BIGINT,
-        likes_po_creation_date VARCHAR(255),
-        PRIMARY KEY (src, dst)
-        )"""
-cursor.execute(sql)
-
-print("Created edge table: likes_post")
-
-cursor.execute("DROP TABLE IF EXISTS studyat")
-sql = """CREATE TABLE studyat (
-        src BIGINT,
-        dst BIGINT,
-        sa_class_year INT,
-        PRIMARY KEY (src, dst)
-        )"""
-cursor.execute(sql)
-
-print("Created edge table: studyat")
-
-cursor.execute("DROP TABLE IF EXISTS workat")
-sql = """CREATE TABLE workat (
-        src BIGINT,
-        dst BIGINT,
-        wa_work_from INT,
-        PRIMARY KEY (src, dst)
-        )"""
-cursor.execute(sql)
-
-print("Created edge table: workat")
-
-conn.commit()
-
-print("\n========================================\n")
-if args.db_type == "mysql":
-    sql = f"SHOW TABLES FROM {args.db}"
-    cursor.execute(sql)
-    print("Execute SQL: " + sql)
-    print("-----------------------------------------")
-    results = cursor.fetchall()
-    for row in results:
-        print(row[0])
-elif args.db_type == "postgresql":
-    cursor.execute(
-        "SELECT table_name FROM information_schema.tables WHERE table_schema = 'public'"
-    )
-    # Fetch all the results
-    results = cursor.fetchall()
-    # Print the table names
-    for row in results:
-        print(row[0])
-
-print(f"Done creating {len(results)} tables")
-
-conn.close()
+# Function to create a table
+def create_table(cursor, table_name, sql_statement, vertex=True):
+    cursor.execute(f"DROP TABLE IF EXISTS {table_name}")
+    cursor.execute(sql_statement)
+    prefix = "Created vertex" if vertex else "Created edge"
+    print(f"{prefix} table: {table_name}")
+
+
+def main():
+    args = get_args()
+    print("Args: ", args)
+    print("\n========================================\n")
+
+    if args.db_type == "mysql":
+        connection_string = "mysql+pymysql://%s:%s@%s:%s/" % (
+            args.user,
+            args.password,
+            args.host,
+            args.port,
+        )
+        engine = create_engine(connection_string, echo=False)
+    elif args.db_type == "postgresql":
+        connection_string = "postgresql://%s:%s@%s:%s/%s" % (
+            args.user,
+            args.password,
+            args.host,
+            args.port,
+            args.db,
+        )
+        engine = create_engine(connection_string, echo=False)
+    else:
+        print("We now only support mysql and postgresql")
+        exit(1)
+
+    conn = engine.raw_connection()
+    cursor = conn.cursor()
+
+    # create the database
+    if args.drop:
+        print(f"Drop database {args.db}")
+        if args.db_type == "mysql":
+            cursor.execute(f"DROP DATABASE IF EXISTS {args.db}")
+        elif args.db_type == "postgresql":
+            print("Not supported drop database for postgresql.")
+
+    if args.db_type == "mysql":
+        cursor.execute(f"CREATE DATABASE IF NOT EXISTS {args.db}")
+        cursor.execute(f"USE {args.db}")
+    # Not supported create database for postgresql
+
+    # create vertex tables
+    for table_name, sql_statement in vertexTableDDL.items():
+        create_table(cursor, table_name, sql_statement)
+
+    # insert edge tables
+    for table_name, sql_statement in simpleEdgeTableDDL.items():
+        create_table(cursor, table_name, sql_statement, False)
+
+    for table_name, sql_statement in propertyEdgeTableDDL.items():
+        create_table(cursor, table_name, sql_statement, False)
+
+    conn.commit()
+
+    print("\n========================================\n")
+    if args.db_type == "mysql":
+        sql = f"SHOW TABLES FROM {args.db}"
+        cursor.execute(sql)
+        print("Execute SQL: " + sql)
+        print("-----------------------------------------")
+        results = cursor.fetchall()
+        for row in results:
+            print(row[0])
+    elif args.db_type == "postgresql":
+        cursor.execute(
+            "SELECT table_name FROM information_schema.tables WHERE table_schema = 'public'"
+        )
+        # Fetch all the results
+        results = cursor.fetchall()
+        # Print the table names
+        for row in results:
+            print(row[0])
+
+    print(f"Done creating {len(results)} tables")
+
+    conn.close()
+
+
+if __name__ == "__main__":
+    main()
